50925c641005557084a1b396abe61113b1fd7c7c
Eric Garrison <egarrison@gmail.com>
2019-09-09 09:11:48 -0500

    Updated post-build.js file for the Angular Library
    This is in preparation for the future Angular Library project that will be coming soon.

--- post-build.js
+++ post-build.js
@@ -3,17 +3,27 @@
  * https://blog.nodeswat.com/automagic-reload-for-clients-after-deploy-with-angular-4-8440c9fdd96c
  *
  * Modified and Adapted By: Eric Garrison
+ * Updated for future Angular Library support on 9/9/2019
  */
 const path = require('path')
 const fs = require('fs')
 const util = require('util')
 const moment = require('moment')
 
-// get application version from package.json
-//const appVersion = require('../package.json').version
-const versionFromFile = require('../dist/version.json').version
+process.argv.forEach((val, index) => {
+  console.log(`${index}: ${val}`)
+})
 
-// promisify core API's
+let project = process.argv[2]
+let basePath = path.join(__dirname, `../../../../dist/${project}/`)
+
+console.log(`\nProject: ${project}`)
+console.log(`Base Path: ${basePath}`)
+
+// Get application version from package.json
+const versionFromFile = require(`${basePath}version.json`).version
+
+// Promisify core API's
 const readDir = util.promisify(fs.readdir)
 const writeFile = util.promisify(fs.writeFile)
 const readFile = util.promisify(fs.readFile)
@@ -21,14 +31,14 @@ const readFile = util.promisify(fs.readFile)
 console.log('\nRunning post-build tasks')
 
 // our version.json will be in the dist folder
-const versionFilePath = path.join(__dirname + '/../dist/version.json')
+const versionFilePath = `${basePath}version.json`
+
+console.log(`Version File Path: ${basePath}`)
 
 let mainHash = ''
 let mainBundleFile = ''
 
 // RegExp to find main-es2015.bundle.js, even if it doesn't include a hash in it's name (dev build)
-// Note: If you are using a version of Angular lower than Angular 8, the file will start with "main.",
-// as main-es2015 is part of the new build output for Angular 8.
 let mainBundleRegexp = /^main-es2015.?([a-z0-9]*)?.js$/
 
 // Update version number with the current date of the build
@@ -55,7 +65,7 @@ if (versionFromFile === appVersion) {
 console.log(`Changed Version To: ${appVersion}`)
 
 // read the dist folder files and find the one we're looking for
-readDir(path.join(__dirname, '../dist/'))
+readDir(`${basePath}`)
   .then(files => {
     mainBundleFile = files.find(f => mainBundleRegexp.test(f))
 
@@ -84,7 +94,7 @@ readDir(path.join(__dirname, '../dist/'))
     console.log(`Replacing hash in the ${mainBundleFile}`)
 
     // Replace the hash and version placeholders in our main.js file so the code knows the current hash and version
-    const mainFilepath = path.join(__dirname, '../dist/', mainBundleFile)
+    const mainFilepath = `${basePath}${mainBundleFile}`
 
     return readFile(mainFilepath, 'utf8').then(mainFileData => {
       console.log(`Application Version: ${appVersion}`)
